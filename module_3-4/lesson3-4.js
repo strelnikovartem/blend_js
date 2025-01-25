efddf1d907c0511fc70be6e82be947bbf362cf47
Manches <Lukjanovilly@gmail.com>
2025-01-25 19:28:43 +0200

    add-task-2

--- module_3-4/lesson3-4.js
+++ module_3-4/lesson3-4.js
@@ -4,19 +4,40 @@
 //Додай перевірку що функція отримує масив
 // const numbers = [2, 5, 35, 56, 12, 1, 7, 80, 3];
 
-const numbers = [2, 5, 35, 56, 12, 1, 7, 80, 3];
-function findSmallerNumber(numbers) {
-  // return Math.min(...numbers);
-  if (!Array.isArray(numbers)) {
-    return "Hello Vova - це не масив";
-  }
-  let numberMin = numbers[0];
-  for (const number of numbers) {
-    if (numberMin > number) {
-      numberMin = number;
-    }
-  }
-  return numberMin;
-}
+// const numbers = [2, 5, 35, 56, 12, 1, 7, 80, 3];
+// function findSmallerNumber(numbers) {
+//   // return Math.min(...numbers);
+//   if (!Array.isArray(numbers)) {
+//     return "Hello Vova - це не масив";
+//   }
+//   let numberMin = numbers[0];
+//   for (const number of numbers) {
+//     if (numberMin > number) {
+//       numberMin = number;
+//     }
+//   }
+//   return numberMin;
+// }
 
-console.log(findSmallerNumber(numbers));
+// console.log(findSmallerNumber(numbers));
+
+//TODO: № 2 =============================================================
+//Напишіть функцію caclculateAverage()
+//яка приймає довільну кількість
+//аргументів і повертає їхнє середнє значення.
+//Додати перевірку, що аргументи це числа.
+
+// function calculateAverage(...args) {
+//   let total = 0;
+//   let counter = 0;
+//   for (const number of args) {
+//     if (typeof number !== "number") {
+//       continue
+//     }
+//     total += number;
+//     counter++;
+//   }
+//   return total / counter;
+// }
+
+// console.log(calculateAverage(10, 10, 20, 200, "hello"));
