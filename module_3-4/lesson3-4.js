e64faaae6eed7e8ee4e3b7eabe8e1236606fc498
Ruzanna <pogosian.ruz@gmail.com>
2025-01-25 18:11:21 +0100

    add for of

--- module_3-4/lesson3-4.js
+++ module_3-4/lesson3-4.js
@@ -6,7 +6,17 @@
 
 const numbers = [2, 5, 35, 56, 12, 1, 7, 80, 3];
 function findSmallerNumber(numbers) {
-  return Math.min(...numbers);
+  // return Math.min(...numbers);
+  if (!Array.isArray(numbers)) {
+    return "Hello Vova - це не масив";
+  }
+  let numberMin = numbers[0];
+  for (const number of numbers) {
+    if (numberMin > number) {
+      numberMin = number;
+    }
+  }
+  return numberMin;
 }
 
 console.log(findSmallerNumber(numbers));
